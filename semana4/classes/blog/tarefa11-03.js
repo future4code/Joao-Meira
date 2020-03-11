36a0d12260e123b8fde16aea6e12c1ccc341d3c4
Joao Meira <jhalvesmeira@gmail.com>
2020-03-11 14:41:58 -0300

    obrigatoria passo 3

--- semana4/classes/blog/tarefa11-03.js
+++ semana4/classes/blog/tarefa11-03.js
@@ -10,5 +10,22 @@ class post {
 
 function criarPost()
 {
-    const meuPost = new post (titulo, autor, texto)
+    
+    variosPosts = [];
+    console.log(variosPosts);
+
+    const titulo = document.getElementById("titulo");
+    const autor = document.getElementById("autor");
+    const texto = document.getElementById("texto");
+    console.log(titulo.value, autor.value, texto.value);
+
+    const meuPost = new post (titulo.value, autor.value, texto.value);
+    variosPosts.push(meuPost);
+
+
+    titulo.value = ""
+    autor.value = ""
+    texto.value = ""
+    
+
 }
