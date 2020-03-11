6427cd68d3ace0b39cc138ebf840746c564f0c85
Joao Meira <jhalvesmeira@gmail.com>
2020-03-11 14:06:53 -0300

    obrigatoria passo 2

---
+++ semana4/classes/blog/tarefa11-03.js
@@ -0,0 +1,14 @@
+class post {
+    constructor (titulo, autor, texto)
+    {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.texto = texto;
+    }
+
+}
+
+function criarPost()
+{
+    const meuPost = new post (titulo, autor, texto)
+}
