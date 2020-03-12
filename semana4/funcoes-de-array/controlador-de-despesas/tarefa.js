48113a5bcf4177df2641efab1e5fb0173c46cd64
Joao Meira <jhalvesmeira@gmail.com>
2020-03-12 15:25:27 -0300

    obrigatoria passo 3, 4 e 5

--- semana4/funcoes-de-array/controlador-de-despesas/tarefa.js
+++ semana4/funcoes-de-array/controlador-de-despesas/tarefa.js
@@ -1,3 +1,5 @@
+//Classe
+
 class despesa {
     constructor (valor, tipo, descricao)
     {
@@ -9,10 +11,33 @@ class despesa {
 }
 
 const listaDespesas =[]
-console.log(listaDespesas)
 
 function cadastrarDespesa()
 {
-    const minhaDespesa = new despesa (document.getElementById("valor").value, document.getElementById("tipo").value, document.getElementById("descricao").value);
-    listaDespesas.push(minhaDespesa)
+    const valor = document.getElementById("valor")
+    const tipo = document.getElementById("tipo")
+    const descricao = document.getElementById("descricao")
+    
+    //Verificador
+    const minhaDespesa = new despesa (valor.value, tipo.value, descricao.value);
+    if(valor.value === "" || tipo.value === "" || descricao.value === "" || valor.value === "0"){
+        alert("preenche direito esse negócio")
+    } else
+    {
+        listaDespesas.push(valor.value, tipo.value, descricao.value)
+    }
+    console.log(listaDespesas)
+
+    //Adicionar lista
+    const listaDetalhada = document.getElementById("lista-detalhada")
+    //listaDetalhada.innerHTML += "<li> Despesa com: " + tipo.value + " - " + descricao.value + " - R$" + valor.value + "</li>"
+    listaDetalhada.innerHTML += "<li> Despesa com: " + listaDespesas[1] + " - " + listaDespesas[2] + " - R$" + listaDespesas[0] + "</li>"
+
+
+    //Apagar valores
+    valor.value = ""
+    tipo.value = ""
+    descricao.value = ""
+
 }
+
