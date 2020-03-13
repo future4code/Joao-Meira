a90d1053fe8a918f31092131bb19d15e1f0d03b7
Joao Meira <jhalvesmeira@gmail.com>
2020-03-13 14:23:41 -0300

    Exercicios de interpretação e lógica de programação

---
+++ semana4/lista-de-exercicios/projeto-13-03.js
@@ -0,0 +1,230 @@
+//  INTERPRETAÇÃO DE CÓDIGO
+
+/*
+EXERCÍCIO 1
+
+A função inicia criando uma variável que referencia o valor numérico informado pelo usuário em um prompt.
+Essa função realiza a conversão de um valor em dólar para reais pela cotação informada no prompt.
+O valor em dolar é informado através da constante meuDinheiro, que atribuiu valor 100 a valorEmDolar.
+Ao fim é executado um console.log que imprime o valor no console do navegador.
+
+
+
+
+EXERCÍCIO 2
+
+A função investeDinheiro trata de investimento de dinheiro através dos argumentos tipoDeInvestimento e valor.
+Uma sentença do tipo switch é criada para oferecer valores variados a cada uma das opções de investimentos,
+tendo como resultado a atribuição de valor à constante valorAposInvestimento pela multiplicação do valor
+investido sobre a expectativa de rendimento do investimento.
+Caso o usuário escolha um tipo de investimento não existente nas opções tratadas pela sentença "switch", é exibido um
+alerta de "TIPO DE INVESTIMENTO INFORMADO INCORRETO!"
+A função investeDinheiro retorna o valor da constante valorAposInvestimento.
+
+Em sequência são determinadas duas constantes com atribuição de dois investimentos diferentes: (i) novoMontante - que escolhe a opção de 150 reais para investimento
+em ações; (ii) segundoMontante - que escolhe a opção de Tesouro Direto para investir a quantia de 200 reais.
+
+Ao fim é impresso no console do navagador o valor de novoMontente (que será: "165") e, abaixo, a mensagem "undefined" pela não atribuição de valor a segundoMontante, vez que
+inexistente a opção de "Tesoudo Direto" na sentença "switch".
+
+
+
+
+EXERCÍCIO 3
+O código inicia com a criação de 3 constantes: numeros, array1 e array2.
+Em seguida há a formulação de uma verificação "for of" para determinar os números pares da array "numeros" (que serão armazenados em "array1") e os numeros ímpares dessa mesma
+constante (que serão armazenados na "array2").
+
+Por fim o código imprime no console do navegador a mensagem "Quantidade total de números 14", "6" e "8", um abaixo do outro, consecutivamente.
+
+
+
+EXERCÍCIO 4
+O código inicia com a criação de uma constante rígida com identificação de "numeros", e duas variáveis do tipo "let: numero1 e numero2".
+
+Como tarefa seguinte, o código cria uma verificação "for of" para delimitar os elementos da array "numeros" que são menores que a constante
+"numero1" - por uma condição; e os elementos maiores que "numero2" - por outra condição. 
+Como "numero1" tem valor "Infinity", a essa constante serão adicionados todos os valores da array numeros, vez que "Infinity" é o maior valor
+possível.
+"numero 2", por sua vez, terá uma array composta por quase todos os elementos de "numeros", com exceção do elemento -10.
+
+Ao fim, serão impressos no console do navegador a array numero1 e numero2, uma abaixo da outra, consecutivamente.
+*/
+
+
+
+
+
+// EXERCÍCIO DE LÓGICA DE PROGRAMAÇÃO
+
+/*
+1 - Cite 3 maneiras de se percorrer/iterar uma lista. Faça um programa para exemplificar.
+*/
+/*
+array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for(numero of array)
+{
+    if(numero%2 === 0)
+    {
+        console.log("O número " + numero + " é par")
+    } else {
+        console.log("O número " + numero + " é ímpar")
+    }
+}
+
+
+for(i=0; i<array.length; i++)
+{
+    let impresso=array[i]
+    console.log(impresso)
+}
+
+
+array.forEach((numero, index, array) =>
+    console.log(numero)
+)
+*/
+
+
+
+
+/*
+2 - Para este exercício considere as seguintes variáveis:
+    const booleano1 = true
+    const booleano2 = false
+    const booleano3 = !booleano2 (true)
+    const booleano4 = !booleano3 (false)
+
+Sem rodar nenhum código, diga quais são os valores das expressões lógicas abaixo:
+
+a) `booleano1 && booleano2 && !booleano4`
+False
+
+b) `(booleano1 && booleano2) || !booleano3`
+False
+
+c)  `(booleano2 || booleano3) && (booleano4 || booleano1)`
+True
+
+d) `!(booleano2 && booleano3) || !(booleano1 && booleano3)`
+True
+
+e) `!(booleano1) && !(booleano3) || (!booleano4 && booleano3 && booleano3)`
+True
+*/
+
+
+
+
+
+
+
+
+/*
+3 - Você tem que escrever um código que, dado um número N, ele imprima (no console) os N primeiros números pares 
+(por exemplo, se N for 3, você deve imprimir 0, 2 e 4; se N for 5, deve imprimir 0, 2, 4, 6 e 8).  
+Um colega seu disse que já começou esta tarefa, mas não conseguiu terminar. Dê uma olhada no código dele:
+
+const quantidadeDeNumerosPares
+let i = 0
+while(i <= quantidadeDeNumerosPares) {
+  console.log(i*2)
+}
+
+Este código funciona? Por quê? Caso não funcione, corrija a implementação dele.
+    Resposta: não e o motivo principal é que não há atribuição de valores à const quantidadeDeNumerosPares.
+    Pela inexistência do valor, não há como realizar a operação while, vez que i não possui valor com que
+    se comparar. Contudo, para percorrer o array verificando os números, acredito que o melhor a se fazer
+    seja o uso da sentença "for", atribuindo como quantidade de números a serem percorridos na array o dobro
+    do valor N.
+    A minha correção ficaria assim:
+*/
+/*
+numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+N = 3
+percorrer = N * 2
+for(i=0; i < percorrer ; i++)
+{
+    if (numeros[i] %2 === 0)
+    console.log(numeros[i])
+}
+*/
+
+
+
+/*
+4 - Vocês lembram de trigonometria? (Oh, não, trigonometria). Relaxem. O exercício é simples, mas mexe com isso. 
+Veja bem: quando nos ensinam triângulos (uma figura de três lados), nós aprendemos como classifica-los dependendo do tamanho de seus lados. 
+Se um triângulo possuir os três lados iguais, ele é chamado de "Equilátero". 
+Se possuir, dois (e somente 2) lados iguais, diz-se que ele é "Isósceles". 
+Se os três lados tiverem medidas diferentes, ele é "Escaleno". 
+Faça uma função que receba como parâmetro os tamanhos dos três lados do triângulo: a, b, c  e retorne se ele é equilátero, isósceles ou escaleno.
+*/
+/*
+let queTipoDeTriangulo = (ladoA, ladoB, ladoC) =>
+{
+    if (ladoA === ladoB && ladoA === ladoC && ladoB === ladoC)
+    {
+        console.log("O triángulo é equilátero")
+    }
+    else if (ladoA !== ladoB && ladoA !== ladoC && ladoB !== ladoC)
+    {
+        console.log("O triángulo é escaleno")
+    } else
+    {
+        console.log("O triángulo é isóceles")
+    }
+}
+
+queTipoDeTriangulo(2, 1, 3)
+queTipoDeTriangulo(2, 2, 2)
+queTipoDeTriangulo(3, 1, 3)
+*/
+/*
+5 - Faça um programa que, dados dois números,
+i. indique qual é o maior,
+
+ii. determine se eles são divisíveis um pelo outro (use o operador `%`) e
+
+iii. determine a diferença entre eles (o resultado deve ser um número positivo sempre)
+*/
+/*
+let compararDoisNumeros = (num1, num2) =>
+{    
+    if (num1>num2)
+    {
+        console.log(num1 + " é maior que " + num2)
+
+    } else
+    {
+        console.log(num2 + " é maior que " + num1)
+    }
+
+    if(num1%num2 === 0)
+    {
+        console.log(num1 + " é divisível por " + num2)
+    } else if (num2%num1 === 0)
+    {
+        console.log(num2 + " é divisível por " + num1)
+    } else
+    {
+        console.log("Os números não são divisíveis entre si")
+    }
+
+    const diferença = num1-num2
+    if (diferença >= 0)
+    {
+        console.log("A diferença entre os números " + num1 + " e " + num2 + " é " + diferença)
+    }else
+    {
+        x = Number(diferença) * -1
+        console.log("A diferença entre os números " + num1 + " e " + num2 + " é " + x)
+    }
+}
+
+compararDoisNumeros(2, 4)
+compararDoisNumeros(4, 2)
+compararDoisNumeros(3, 4)
+compararDoisNumeros(4, 3)
+*/
