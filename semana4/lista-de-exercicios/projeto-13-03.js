c9f5d21ce16af5674d27105bd398e39fa370ce30
Joao Meira <jhalvesmeira@gmail.com>
2020-03-14 11:52:18 -0300

    exercício 2 e 3 da função de arrays

--- semana4/lista-de-exercicios/projeto-13-03.js
+++ semana4/lista-de-exercicios/projeto-13-03.js
@@ -384,8 +384,10 @@ console.log (filtroDeJovens)
 
 
 
+
+
 /*
-1. Em todos os itens deste exercício, você terá que escrever uma função cuja entrada seja um array. 
+2. Em todos os itens deste exercício, você terá que escrever uma função cuja entrada seja um array. 
 Para testes, considere: const array = [1, 2, 3, 4, 5, 6].
 a) Escreva uma função que retorne um array com as entradas multiplicadas por 2. 
 Isto é [2, 4, 6, 8, 10, 12].
@@ -394,7 +396,7 @@ Isto é: ["3", "6", "9", "15", "18"]
 c) Escreva uma função que retorne um array de strings dizendo: "${número} é par/impar". 
 Isto é: ["1 é impar", "2 é par", "3 é impar", "4 é par", "5 é impar", "6 é par"] 
 */
-
+/*
 const array = [10, 32, 13, 64, 95, 67]
 console.log(array)
 
@@ -437,3 +439,53 @@ function tornarString(){
 }
 
 tornarString()
+*/
+
+
+
+
+/*
+3. Imagine que você trabalhe num parque de diversões, como desenvolvedor(a). 
+As suas tarefas são sempre com o intuito de ajudar a automação de alguns processos internos do parque. 
+Uma das atrações principais dele é a montanha russa do terror. As filas são muito grandes e todas as pessoas 
+de várias idades insistem entrar no brinquedo, mesmo sabendo que não podem. Considere, então, essas pessoas:
+
+const pessoas = [
+	{ nome: "Paula", idade: 12, altura: 1.8},
+	{ nome: "João", idade: 20, altura: 1.3},
+	{ nome: "Pedro", idade: 15, altura: 1.9},
+	{ nome: "Luciano", idade: 22, altura: 1.8},
+	{ nome: "Artur", idade: 10, altura: 1.2},
+	{ nome: "Soter", idade: 70, altura: 1.9}
+]
+
+A regra para entrar na montanha russa do terror é: 
+ter, no mínimo, 1.5m de altura; ser mais velho do que 14 anos e mais novo do que 60 anos.
+a) Escreva uma função que receba este array e devolva outro array somente com as pessoas que tem permissão de entrar no brinquedo
+b) Escreva uma função que receba este array e devolva outro array somente com as pessoas que não podem entrar no brinquedo.
+*/
+const pessoas = [
+	{ nome: "Paula", idade: 12, altura: 1.8},
+	{ nome: "João", idade: 20, altura: 1.3},
+	{ nome: "Pedro", idade: 15, altura: 1.9},
+	{ nome: "Luciano", idade: 22, altura: 1.8},
+	{ nome: "Artur", idade: 10, altura: 1.2},
+	{ nome: "Soter", idade: 70, altura: 1.9}
+]
+
+const permitidoEntrar = pessoas.map((pessoa, index, array)=>
+{
+    if(pessoa.idade >= 14 && pessoa.idade < 60 && pessoa.altura >= 1.5){
+        return pessoa
+    }
+})
+console.log(permitidoEntrar)
+
+const proibidoEntrar = pessoas.map((pessoa, index, array)=>
+{
+    if(pessoa.idade < 14 || pessoa.idade > 60 || pessoa.altura < 1.5)
+    {
+        return pessoa
+    }
+})
+console.log(proibidoEntrar)
