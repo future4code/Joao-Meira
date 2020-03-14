c81425896e06e3ca604d1f3cf880337d062e5a88
Joao Meira <jhalvesmeira@gmail.com>
2020-03-14 15:52:29 -0300

    exercicio 5. Fim das atividades

--- semana4/lista-de-exercicios/projeto-13-03.js
+++ semana4/lista-de-exercicios/projeto-13-03.js
@@ -516,7 +516,7 @@ Olá, ${ Sr./Sra. } { nome da pessoa }. Infelizmente, sua consulta marcada
 para o dia ${ data da consulta } foi cancelada. Se quiser, pode entrar em 
 contato conosco para remarcá-la
 */
-
+/*
 const consultas = [
 	{ nome: "João", genero: "masculino", cancelada: true, dataDaConsulta: "01/10/2019" },
 	{ nome: "Pedro", genero: "masculino", cancelada: false, dataDaConsulta: "02/10/2019" },
@@ -563,6 +563,55 @@ function imprimirNoDiv(){
         document.getElementById("container").innerHTML += "<p>" + email + "</p>"
     }
 }
+*/
 
 
+
+
+
+
+/*
+5. Agora, pediram para você ajudar a fazer uma funcionalidade de um banco digital. 
+Antes de explicar a sua tarefa, você precisa entender como eles guardam as contas dos clientes. 
+Basicamente, eles salvam o nome do clientes, o saldo total e uma lista contendo todas as compras realizadas pelo cliente. 
+Veja abaixo:
+const contas = [
+	{ cliente: "João", saldoTotal: 1000, compras: [100, 200, 300] },
+	{ cliente: "Paula", saldoTotal: 7500, compras: [200, 1040] },
+	{ cliente: "Pedro", saldoTotal: 10000, compras: [5140, 6100, 100, 2000] },
+	{ cliente: "Luciano", saldoTotal: 100, compras: [100, 200, 1700] },
+	{ cliente: "Artur", saldoTotal: 1800, compras: [200, 300] },
+	{ cliente: "Soter", saldoTotal: 1200, compras: [] }
+]
+A sua tarefa é: faça uma função que receba um array com os objetos do tipo acima e atualize o saldo total individual de cada um, 
+sem criar um novo array.
+Em outras palavras, o array do exemplo acima deve ficar assim:
+
+[ 
+    	{ cliente: 'João', saldoTotal: 400, compras: [ 100, 200, 300 ] },
+    	{ cliente: 'Paula', saldoTotal: 6260, compras: [ 200, 1040 ] },
+      { cliente: 'Pedro', saldoTotal: -3340, compras: [ 5140, 6100, 100, 2000 ] },
+      { cliente: 'Luciano', saldoTotal: -1900, compras: [ 100, 200, 1700 ] },
+      { cliente: 'Artur', saldoTotal: 1300, compras: [ 200, 300 ] },
+      { cliente: 'Soter', saldoTotal: 1200, compras: [] } 
+    ]
+*/
+
+const contas = [
+	{ cliente: "João", saldoTotal: 1000, compras: [100, 200, 300] },
+	{ cliente: "Paula", saldoTotal: 7500, compras: [200, 1040] },
+	{ cliente: "Pedro", saldoTotal: 10000, compras: [5140, 6100, 100, 2000] },
+	{ cliente: "Luciano", saldoTotal: 100, compras: [100, 200, 1700] },
+	{ cliente: "Artur", saldoTotal: 1800, compras: [200, 300] },
+    { cliente: "Soter", saldoTotal: 1200, compras: [] }
+]
+
+const subtrairComprasDoSaldo = contas.forEach((cliente, index, array)=>{
+    let compras = cliente.compras
+    for(cadaCompra of compras){
+       cliente.saldoTotal = cliente.saldoTotal - cadaCompra
+    }  
+})
+
+console.log(contas)
 
