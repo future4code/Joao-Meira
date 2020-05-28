2a161b0879d5851c098249fa0d709961900b70b3
Meira-JH <jhalvesmeira@gmail.com>
2020-05-28 16:43:51 -0300

    index funcionando corretameente

--- semana14/projeto-sistema-bancario/build/index.js
+++ semana14/projeto-sistema-bancario/build/index.js
@@ -3,16 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const getAllAccounts_1 = require("./getAllAccounts");
 const createAccount_1 = require("./createAccount");
 const checkBalance_1 = require("./checkBalance");
-const addCredit_1 = require("./addCredit");
-const transferToAccount_1 = require("./transferToAccount");
-const paymentDebit_1 = require("./paymentDebit");
-const actionChoice = process.argv[2];
+const actionType = process.argv[2];
 const firstArgument = process.argv[3];
 const secondArgument = process.argv[4];
 const thirdArgument = process.argv[5];
 const fourthArgument = process.argv[6];
 const fifthArgument = process.argv[7];
-switch (actionChoice) {
+switch (actionType) {
     case "GET_ALL_ACCOUNTS":
         getAllAccounts_1.getAllAccounts();
         break;
@@ -22,17 +19,7 @@ switch (actionChoice) {
     case "CHECK_BALANCE":
         checkBalance_1.checkBalance(firstArgument, secondArgument);
         break;
-    case "ADD_CREDIT":
-        addCredit_1.addCredit(firstArgument, secondArgument);
-        break;
-    case "TRANSFER_TO_ACCOUNT":
-        transferToAccount_1.transferToAccount(firstArgument, secondArgument, thirdArgument, fourthArgument, fifthArgument);
-        break;
-    case "DEBIT":
-        paymentDebit_1.paymentDebit(firstArgument, secondArgument, thirdArgument, fourthArgument);
-        break;
     default:
         console.log('Ação não encontrada!');
-        break;
 }
 //# sourceMappingURL=index.js.map
--- semana14/projeto-sistema-bancario/src/index.ts
+++ semana14/projeto-sistema-bancario/src/index.ts
@@ -6,50 +6,29 @@ import { addCredit } from './addCredit'
 import { transferToAccount } from './transferToAccount'
 import { paymentDebit } from './paymentDebit'
 
-const selectedAction = (
-    actionType : string, 
-    argument1? : string, 
-    argument2? : any, 
-    argument3? : string, 
-    argument4? : any, 
-    argument5? : any, 
-    ) => {
-    switch (actionType) {
-        case "GET_ALL_ACCOUNTS":
-            getAllAccounts();
-            break;
-        case "CREATE_ACCOUNT":
-            createAccount(argument1, argument2, argument3);
-            break;
-        case "CHECK_BALANCE":
-            checkBalance(argument1, argument2);
-            break;
-        case "ADD_CREDIT":
-            addCredit(argument1, argument2);
-            break;
-        case "TRANSFER_TO_ACCOUNT":
-            transferToAccount(argument1, argument2, argument3, argument4, argument5);
-            break;
-        case "DEBIT":
-            paymentDebit(argument1, argument2, argument3, argument4);
-            break;
-        default:
-            console.log('Ação não encontrada!')
-    }
+
+const actionType : string = process.argv[2];
+
+
+switch (actionType) {
+    case "GET_ALL_ACCOUNTS":
+        getAllAccounts();
+        break;
+    case "CREATE_ACCOUNT":
+        createAccount(process.argv[3], process.argv[4], process.argv[5]);
+        break;
+    case "CHECK_BALANCE":
+        checkBalance(process.argv[3], process.argv[4]);
+        break;
+    case "ADD_CREDIT":
+        addCredit( process.argv[3], Number(process.argv[4]) );
+        break;
+    case "TRANSFER_TO_ACCOUNT":
+        transferToAccount( process.argv[3], process.argv[4], process.argv[5], process.argv[6], Number(process.argv[7]) );
+        break;
+    case "DEBIT":
+        paymentDebit(process.argv[3], process.argv[4], process.argv[5], Number(process.argv[6]) );
+        break;
+    default:
+        console.log('Ação não encontrada!')
 }
-
-const actionChoice : string = process.argv[2];
-const firstArgument = process.argv[3];
-const secondArgument = process.argv[4];
-const thirdArgument = process.argv[5];
-const fourthArgument = process.argv[6];
-const fifthArgument = process.argv[7];
-
-selectedAction(
-    actionChoice, 
-    firstArgument, 
-    secondArgument, 
-    thirdArgument, 
-    fourthArgument,
-    fifthArgument
-    )
