4872dd2da79ef3eab4a849365459b9cc8b0c9b32
Meira-JH <jhalvesmeira@gmail.com>
2020-06-11 16:44:27 -0300

    tarefas obrigatórias feitas

---
+++ semana17/quinta/src/requisitions.ts
@@ -0,0 +1,21 @@
+import { AddressInfo } from "net";
+import { 
+
+} from "./queries";
+import express, { Request, Response } from "express";
+
+export const app = express();
+
+app.use(express.json());
+
+
+
+
+    export const server = app.listen(process.env.DB_PORT || 3000, () => {
+        if (server) {
+          const address = server.address() as AddressInfo;
+          console.log(`Server is running in http://localhost:${address.port}`);
+        } else {
+          console.error(`Failure upon starting server.`);
+        }
+      });
